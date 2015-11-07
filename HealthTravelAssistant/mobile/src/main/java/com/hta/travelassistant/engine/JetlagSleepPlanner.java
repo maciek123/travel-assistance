fd25e280413fc8898bf1a1d2ab05cd6744259ab8
Maciej Kazirod <Maciej.Kazirod@swisscom.com>
2015-11-08 00:45:50 +0100

    refcatoring

--- HealthTravelAssistant/mobile/src/main/java/com/hta/travelassistant/engine/JetlagSleepPlanner.java
+++ HealthTravelAssistant/mobile/src/main/java/com/hta/travelassistant/engine/JetlagSleepPlanner.java
@@ -1,6 +1,5 @@
 package com.hta.travelassistant.engine;
 
-import com.hta.travelassistant.model.Action;
 import com.hta.travelassistant.model.FlightInfo;
 import com.hta.travelassistant.model.Recommendation;
 import com.hta.travelassistant.model.SleepEntry;
@@ -10,8 +9,6 @@ import org.joda.time.DateTimeZone;
 import org.joda.time.Duration;
 import org.joda.time.LocalTime;
 
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -20,7 +17,8 @@ import static com.hta.travelassistant.model.Action.NOSUNLIGHT;
 import static com.hta.travelassistant.model.Action.SLEEP;
 import static com.hta.travelassistant.model.Action.SUNLIGHT;
 import static java.util.Arrays.asList;
-import static java.util.Collections.*;
+import static java.util.Collections.singletonList;
+import static org.joda.time.Duration.standardHours;
 
 public class JetlagSleepPlanner implements SleepPlanner {
     @Override
@@ -43,10 +41,10 @@ public class JetlagSleepPlanner implements SleepPlanner {
         while (offset * step < 0 && timeToSleep.isAfterNow()) {
             timeToSleep = timeToSleep.plusMinutes((int) (60 * step));
             if (fwd) {
-                recommendations.add(new Recommendation(timeToSleep.minusHours(3), Duration.standardHours(3), asList(SUNLIGHT, NOSLEEP)));
-                recommendations.add(new Recommendation(timeToSleep.plus(normalDuration), Duration.standardHours(1), asList(NOSUNLIGHT, NOSLEEP)));
+                recommendations.add(new Recommendation(timeToSleep.minusHours(3), standardHours(3), asList(SUNLIGHT, NOSLEEP)));
+                recommendations.add(new Recommendation(timeToSleep.plus(normalDuration), standardHours(1), asList(NOSUNLIGHT, NOSLEEP)));
             } else {
-                recommendations.add(new Recommendation(timeToSleep.plus(normalDuration), Duration.standardHours(3), asList(SUNLIGHT, NOSLEEP)));
+                recommendations.add(new Recommendation(timeToSleep.plus(normalDuration), standardHours(3), asList(SUNLIGHT, NOSLEEP)));
             }
             recommendations.add(new Recommendation(timeToSleep, normalDuration, singletonList(SLEEP)));
             offset += step;
